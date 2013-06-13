b558d016b6e591c993977024ddf4a2b78b627759
Zhang Kefei <zhangkf92@gmail.com>
2013-06-13 22:42:28 +0800

    commit for test version

--- src/com/zju/mobilepaymentapp/data/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.zju.mobilepaymentapp.data;
-
-public class Constants {
-	public static enum TRANS_TYPE{FLIGHT, HOTEL}; 
-	public static enum STATE_TYPE{UNPAID, PAID, SHIPPED, CONFIRMED, CLOSED, IN_REFUND};
-}
